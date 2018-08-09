3d8e64a0b73a8d2d3bba32e2be69fb3c89688b2a
alagarsamya8870 <42169898+alagarsamya8870@users.noreply.github.com>
2018-08-09 12:30:02 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class power
+{
+public static void main(String arg[])
+{
+int a,b,c;
+Scanner s=new Scanner(System.in);
+System.out.println("elements are");
+a=s.nextInt();
+b=s.nextInt();
+System.out.println(Math.pow(a,b));
+}
+}
